9a6d440b6f0e3408b7953ab4c4707e2cbc8728e6
Kirill Minenko <kirill.minenko@sperasoft.com>
2015-10-15 21:31:46 +0300

    Started work with adapter

---
+++ tests/database-connection.js
@@ -0,0 +1,11 @@
+var adapter = require('../index.js');
+
+var assert = require('assert');
+describe('mongo', function() {
+    describe('will try to connect to database', function () {
+        it('should connect', function () {
+
+
+        });
+    });
+});
